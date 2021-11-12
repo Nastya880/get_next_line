2634be004249f080993ef790bfa3678d174f6f41
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2021-11-12 13:16:45 +0300

    Create main.c

---
+++ get_next_line/main.c
@@ -0,0 +1,11 @@
+#include "get_next_line.h"
+
+int main()
+{
+  //int fd;
+  
+  //fd = open("file", O_RDONLY);
+  printf("%s\n", get_next_line(1));
+  //printf("%s\n", get_next_line(fd));
+  //close(fd);
+}
